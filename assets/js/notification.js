02304b4d9506d63ffe02c55fdce83b39895b154b
sungkuk5420 <sungkuk5420@gmail.com>
2019-06-03 19:07:56 +0900

    Connect and Disconnect Notifications

--- assets/js/notification.js
+++
@@ -1,5 +0,0 @@
-const notifications = document.getElementById("jsNotifications");
-
-export const handleNewUser = ({ nickname }) => {
-  console.log(nickname, " just joined");
-};
--- src/socketController.js
+++ src/socketController.js
@@ -1,10 +1,16 @@
 import events from "./events";
 
 const socketController = socket => {
-    socket.on(events.setNickname, ({ nickname }) => {
-      console.log(nickname);
-      socket.nickname = nickname;
-    });
-  };
-  
-  export default socketController;
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
+  socket.on(events.setNickname, ({ nickname }) => {
+    socket.nickname = nickname;
+    broadcast(events.newUser, { nickname });
+  });
+
+  socket.on(events.disconnect, () => {
+    broadcast(events.disconnected, { nickname: socket.nickname });
+  });
+};
+
+export default socketController;
