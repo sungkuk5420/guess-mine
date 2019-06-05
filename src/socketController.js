571e0258138cde9a7e2a2e609ffed2fe2d3778bc
sungkuk5420 <sungkuk5420@gmail.com>
2019-06-05 13:49:52 +0900

    Player Board

--- src/socketController.js
+++ src/socketController.js
@@ -1,17 +1,39 @@
 import events from "./events";
+import { chooseWord } from "./words";
 
-const socketController = socket => {
+let sockets = [];
+let inProgress = false;
+let word = null;
+
+const chooseLeader = () => sockets[Math.floor(Math.random() * sockets.length)];
+
+const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
+  const superBroadcast = (event, data) => io.emit(event, data);
+  const sendPlayerUpdate = () =>
+    superBroadcast(events.playerUpdate, { sockets });
+  const startGame = () => {
+    if (inProgress === false) {
+      inProgress = true;
+      const leader = chooseLeader();
+      word = chooseWord();
+    }
+  };
 
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
+    sockets.push({ id: socket.id, points: 0, nickname: nickname });
     broadcast(events.newUser, { nickname });
+    sendPlayerUpdate();
+    startGame();
   });
 
   socket.on(events.disconnect, () => {
+    sockets = sockets.filter(aSocket => aSocket.id !== socket.id);
     broadcast(events.disconnected, { nickname: socket.nickname });
+    sendPlayerUpdate();
   });
-  
+
   socket.on(events.sendMsg, ({ message }) =>
     broadcast(events.newMsg, { message, nickname: socket.nickname })
   );
