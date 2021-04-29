dfbc9212cff88e62efefec6d5e95c4b543dd321c
sungkuk5420 <sungkuk5420@gmail.com>
2021-04-30 00:02:06 +0900

    유저 연동

--- assets/js/players.js
+++ assets/js/players.js
@@ -7,17 +7,48 @@ import {
 } from "./paint";
 import { disableChat, enableChat } from "./chat";
 
-const board = document.getElementById("jsPBoard");
+
+const userInfo1 = document.querySelector(".user-1");
+const userInfo2 = document.querySelector(".user-2");
+const userInfo3 = document.querySelector(".user-3");
+const userInfo4 = document.querySelector(".user-4");
+const userInfo5 = document.querySelector(".user-5");
+const userInfo6 = document.querySelector(".user-6");
+const userInfo7 = document.querySelector(".user-7");
+const userInfo8 = document.querySelector(".user-8");
 const notifs = document.getElementById("jsNotifs");
 const notifs2 = document.getElementById("jsNotifs2");
 const notifs3 = document.getElementById("jsNotifs3");
 
 const addPlayers = players => {
-  board.innerHTML = "";
+  let index  =1;
   players.forEach(player => {
-    const playerElement = document.createElement("span");
-    playerElement.innerText = `${player.nickname}: ${player.points}`;
-    board.appendChild(playerElement);
+    if(index ==1){
+      userInfo1.querySelector(".name").innerText =player.nickname;
+      userInfo1.querySelector(".score").innerText =player.points;
+    }else if(index ==2){
+      userInfo2.querySelector(".name").innerText =player.nickname;
+      userInfo2.querySelector(".score").innerText =player.points;
+    }else if(index ==3){
+      userInfo3.querySelector(".name").innerText =player.nickname;
+      userInfo3.querySelector(".score").innerText =player.points;
+    }else if(index ==4){
+      userInfo4.querySelector(".name").innerText =player.nickname;
+      userInfo4.querySelector(".score").innerText =player.points;
+    }else if(index ==5){
+      userInfo5.querySelector(".name").innerText =player.nickname;
+      userInfo5.querySelector(".score").innerText =player.points;
+    }else if(index ==6){
+      userInfo6.querySelector(".name").innerText =player.nickname;
+      userInfo6.querySelector(".score").innerText =player.points;
+    }else if(index ==7){
+      userInfo7.querySelector(".name").innerText =player.nickname;
+      userInfo7.querySelector(".score").innerText =player.points;
+    }else if(index ==8){
+      userInfo8.querySelector(".name").innerText =player.nickname;
+      userInfo8.querySelector(".score").innerText =player.points;
+    }
+    index++;
   });
 };
 
