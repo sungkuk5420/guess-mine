ef152ace5fa0ac0ce65a1b211f6a01b76f5a791a
sungkuk5420 <sungkuk5420@gmail.com>
2021-05-01 20:19:30 +0900

    유저 재접속에 따른 동기화 해결

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -11,5 +11,7 @@ const fireNotification = (text, color) => {
 export const handleNewUser = ({ nickname }) =>
   fireNotification(`${nickname} 님이 들어왔습니다!`, "rgb(0, 122, 255)");
 
-export const handleDisconnected = ({ nickname }) =>
+export const handleDisconnected = ({ nickname }) =>{
   fireNotification(`${nickname} 님이 나가셨습니다!`, "rgb(255, 149, 0)");
+}
+  
--- assets/js/players.js
+++ assets/js/players.js
@@ -21,51 +21,75 @@ const notifs3 = document.getElementById("jsNotifs3");
 
 const addPlayers = (players) => {
   let index = 1;
+  userInfo1.classList = "user-li user-1";
+  userInfo1.querySelector(".name").innerText = "";
+  userInfo1.querySelector(".score").innerText = "";
+  userInfo1.querySelector(".user-info__photo").classList.remove("is-show");
+  userInfo2.classList = "user-li user-2";
+  userInfo2.querySelector(".name").innerText = "";
+  userInfo2.querySelector(".score").innerText = "";
+  userInfo2.querySelector(".user-info__photo").classList.remove("is-show");
+  userInfo3.classList = "user-li user-3";
+  userInfo3.querySelector(".name").innerText = "";
+  userInfo3.querySelector(".score").innerText = "";
+  userInfo3.querySelector(".user-info__photo").classList.remove("is-show");
+  userInfo4.classList = "user-li user-4";
+  userInfo4.querySelector(".name").innerText = "";
+  userInfo4.querySelector(".score").innerText = "";
+  userInfo4.querySelector(".user-info__photo").classList.remove("is-show");
+  userInfo5.classList = "user-li user-5";
+  userInfo5.querySelector(".name").innerText = "";
+  userInfo5.querySelector(".score").innerText = "";
+  userInfo5.querySelector(".user-info__photo").classList.remove("is-show");
+  userInfo6.classList = "user-li user-6";
+  userInfo6.querySelector(".name").innerText = "";
+  userInfo6.querySelector(".score").innerText = "";
+  userInfo6.querySelector(".user-info__photo").classList.remove("is-show");
+  userInfo7.classList = "user-li user-7";
+  userInfo7.querySelector(".name").innerText = "";
+  userInfo7.querySelector(".score").innerText = "";
+  userInfo7.querySelector(".user-info__photo").classList.remove("is-show");
+  userInfo8.classList = "user-li user-8";
+  userInfo8.querySelector(".name").innerText = "";
+  userInfo8.querySelector(".score").innerText = "";
+  userInfo8.querySelector(".user-info__photo").classList.remove("is-show");
   players.forEach((player) => {
     if (index == 1) {
-      userInfo1.classList = "user-li user-1";
       userInfo1.classList.add(player.nickname);
       userInfo1.querySelector(".name").innerText = player.nickname;
       userInfo1.querySelector(".score").innerText = player.points;
       userInfo1.querySelector(".user-info__photo").classList.add("is-show");
     } else if (index == 2) {
-      userInfo2.classList = "user-li user-2";
       userInfo2.classList.add(player.nickname);
       userInfo2.querySelector(".name").innerText = player.nickname;
       userInfo2.querySelector(".score").innerText = player.points;
       userInfo2.querySelector(".user-info__photo").classList.add("is-show");
     } else if (index == 3) {
-      userInfo3.classList = "user-li user-3";
       userInfo3.classList.add(player.nickname);
       userInfo3.querySelector(".name").innerText = player.nickname;
       userInfo3.querySelector(".score").innerText = player.points;
       userInfo3.querySelector(".user-info__photo").classList.add("is-show");
     } else if (index == 4) {
-      userInfo4.classList = "user-li user-4";
       userInfo4.classList.add(player.nickname);
       userInfo4.querySelector(".name").innerText = player.nickname;
       userInfo4.querySelector(".score").innerText = player.points;
       userInfo4.querySelector(".user-info__photo").classList.add("is-show");
     } else if (index == 5) {
-      userInfo5.classList = "user-li user-5";
       userInfo5.classList.add(player.nickname);
       userInfo5.querySelector(".name").innerText = player.nickname;
       userInfo5.querySelector(".score").innerText = player.points;
       userInfo5.querySelector(".user-info__photo").classList.add("is-show");
     } else if (index == 6) {
-      userInfo6.classList = "user-li user-6";
       userInfo6.classList.add(player.nickname);
       userInfo6.querySelector(".name").innerText = player.nickname;
       userInfo6.querySelector(".score").innerText = player.points;
       userInfo6.querySelector(".user-info__photo").classList.add("is-show");
     } else if (index == 7) {
-      userInfo7.classList = "user-li user-7";
       userInfo7.classList.add(player.nickname);
       userInfo7.querySelector(".name").innerText = player.nickname;
       userInfo7.querySelector(".score").innerText = player.points;
       userInfo7.querySelector(".user-info__photo").classList.add("is-show");
     } else if (index == 8) {
-      userInfo8.classList = "user-li user-8";
       userInfo8.classList.add(player.nickname);
       userInfo8.querySelector(".name").innerText = player.nickname;
       userInfo8.querySelector(".score").innerText = player.points;
@@ -91,6 +115,7 @@ export const handleAllNotif3 = (text) => {
 
 export const handlePlayerUpdate = ({ sockets }) => addPlayers(sockets);
 export const handleGameStarted = ({ leader }) => {
+  resetCanvas();
   disableCanvas();
   hideControls();
   enableChat();
